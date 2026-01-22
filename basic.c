92af88aa83267546a22fc65a0394264073fb8c6b
Nahid Islam <nahidislam740405@gmail.com>
2026-01-22 20:30:17 +0600

    first commit

---
+++ basic.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main(){
+    char n="N";
+    float num1 = 2.50;
+
+    int a = 10;
+    int b = 20;
+    int c = a+b;
+    printf("%d", c);
+    printf("\n");
+    printf("%c", n);
+    printf("\n");
+    printf("%f", num1);
+    return 0;
+}
---
+++ datatype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float b;
+    char m;
+
+    scanf("%d", &a);
+    scanf("%f", &b);
+    scanf(" %c", &m);
+    printf("%d %f %c", a,b,m);
+
+    return 0;
+}
