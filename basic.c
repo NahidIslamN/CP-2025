9375b0875d6f5bdceb49efa1ed9e4f0c5f82f677
Nahid Islam <nahidislam740405@gmail.com>
2026-01-26 14:23:45 +0600

    Refactor basic.c and update input/output files; add string.cpp for output demonstration

--- basic.c
+++ basic.c
@@ -1,71 +1,25 @@
-#include <stdio.h>
+# include <stdio.h>
 
 int main(){
-    int a, b, c, sum;
+    // int a,b,c,sum;
+    // scanf("%d %d %d", &a, &b, &c);
+    // sum = a+b+c;
+    // printf("%d",sum);
 
-    scanf("%d %d %d", &a,&b,&c);
-    printf("%d %d %d", a,b,c);
-  
+    // int a,b,c;
+    // scanf("%d %d", &a,&b);
+    // c = a-b;
+    // printf("%d", c);
 
-    // ARITHMETICS OPERATOR
-    // sum = a + b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a - b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a * b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a / b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a % b; // VAGSES
-    // printf("%d", sum);
-    // printf("\n");
+    //same as * and /
+    int a,b,c;
+    scanf("%d %d", &a,&b);
+    // printf("%d %d", a,b);
+    c = a%b;
+    printf("%d", c);
 
-
-    // RELATIONAL OPERATOR
-    // sum = a > b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a >= b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a < b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a <= b;
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a == b; 
-    // printf("%d", sum);
-    // printf("\n");
-    // sum = a != b;
-    // printf("%d", sum);
-    // printf("\n");
-    // printf("%d", sum);
-
-
-    //Logical Operator
     
-    // if (a > b && a > c) {
-    //     printf("The largest number is A = %d\n", a);
-    // }
-    // else if (b > a && b > c) {
-    //     printf("The largest number is B = %d\n", b);
-    // }
-    // else if (a == b || b == c || a == c) {
-    //     printf("Two or more numbers are equal. Cannot determine a single largest number.\n");
-    // }
-    // else {
-    //     printf("The largest number is C = %d\n", c);
-    // }
 
-    // if (!(a>b && a>c)) {
-    //     printf("Correct\n");
-    // }else{
-    //     printf("incorrect");
-    // }
+
     return 0;
 }
---
+++ string.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string a = "nahid islam";
+    cout <<a <<endl;
+}
