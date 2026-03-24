7048239e4c96aa4d632969426f06e58203e087f5
Nahid Islam <nahidislam740405@gmail.com>
2026-03-24 14:43:09 +0600

    Add initial implementation of IntegerSequenceDividing.cpp

---
+++ basic.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a;
+    if(!(cin>>a)) return 0;
+    cout<<a<<endl;
+    return 0;
+}
